0c36c1847ba49db23a537b611b84e1b95a016efb
cynicalight <391900788@qq.com>
2026-04-22 11:50:56 +0800

    add: neotree follow_current_file

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -17,6 +17,9 @@ return {
         },
         opts = {
             filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                },
                 filtered_items = {
                     visible = true,
                     hide_dotfiles = false,
