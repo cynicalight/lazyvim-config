9853d9a965ca85187ad8e1df1738aa24da00d169
cynicalight <391900788@qq.com>
2026-07-03 11:09:57 +0800

    Use catppuccin mocha theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -33,8 +33,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "catppuccin", -- 想用 gruvbox-material 就改这里
-      colorscheme = "gruvbox-material",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
