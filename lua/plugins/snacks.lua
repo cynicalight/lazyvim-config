8108cb21e4b191c3298865204efb85b7a81f28ff
cynicalight <391900788@qq.com>
2026-08-13 16:58:00 +0800

    style: refine floating terminal border

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,9 +1,32 @@
 return {
   "folke/snacks.nvim",
+  init = function()
+    local function set_terminal_border_highlight()
+      local float_border = vim.api.nvim_get_hl(0, { name = "FloatBorder", link = false })
+      vim.api.nvim_set_hl(0, "SnacksTerminalBorder", { fg = float_border.fg, bg = "NONE" })
+    end
+
+    local group = vim.api.nvim_create_augroup("SnacksTerminalBorder", { clear = true })
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = group,
+      callback = set_terminal_border_highlight,
+    })
+    set_terminal_border_highlight()
+  end,
   opts = {
     terminal = {
       win = {
         position = "float",
+        border = {
+          { "╭", "SnacksTerminalBorder" },
+          { "─", "SnacksTerminalBorder" },
+          { "╮", "SnacksTerminalBorder" },
+          { "│", "SnacksTerminalBorder" },
+          { "╯", "SnacksTerminalBorder" },
+          { "─", "SnacksTerminalBorder" },
+          { "╰", "SnacksTerminalBorder" },
+          { "│", "SnacksTerminalBorder" },
+        },
       },
     },
     picker = {
